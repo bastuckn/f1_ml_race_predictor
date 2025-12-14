e468c604ecbd0ac8e62ab7da359a0c588f005ad2
Niklas Bastuck <niklas.bastuck@gmail.com>
2025-12-14 19:21:27 +0100

    tiny fix load_session

--- src/data_ingestion/fetch_fastf1.py
+++ src/data_ingestion/fetch_fastf1.py
@@ -4,7 +4,7 @@ from fastf1 import get_session
 # Enable cache (do this ONCE at import time)
 fastf1.Cache.enable_cache("data/raw/fastf1_cache")
 
-def load_session(year: int, round: int, session_name: int):
+def load_session(year: int, round: int, session_name: str):
     """
     session_name: 'FP1', 'FP2', 'FP3', 'Q', 'R'
     """
