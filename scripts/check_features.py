d36a3097cd601e1aea2d749964e2c023c1e9ac23
Niklas Bastuck <niklas.bastuck@gmail.com>
2025-12-18 22:23:29 +0100

    remove feature checker

--- scripts/check_features.py
+++
@@ -1,6 +0,0 @@
-from src.feature_engineering.build_features import build_feature_table
-
-# Pre-weekend sanity check
-df_pre = build_feature_table(
-print("Pre-weekend nulls:")
-print(df_pre.isnull().sum())
