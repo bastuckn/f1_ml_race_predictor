92a1c3268e0a625995289c612f98bafdc24a0d32
Niklas Bastuck <niklas.bastuck@gmail.com>
2025-12-30 00:50:50 +0100

    script for installation, data prep, and training

---
+++ install.sh
@@ -0,0 +1,32 @@
+# Installation
+
+python3 -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+
+# Fill the cache
+# Done step-by-step because of some weird server-side bug
+
+python3 -m scripts.backfill 2020 2020 R
+python3 -m scripts.backfill 2021 2021 R
+python3 -m scripts.backfill 2022 2022 R
+python3 -m scripts.backfill 2023 2023 R
+python3 -m scripts.backfill 2024 2024 R
+python3 -m scripts.backfill 2025 2025 R
+python3 -m scripts.backfill 2026 2026 R
+python3 -m scripts.backfill 2020 2020 Q
+python3 -m scripts.backfill 2021 2021 Q
+python3 -m scripts.backfill 2022 2022 Q
+python3 -m scripts.backfill 2023 2023 Q
+python3 -m scripts.backfill 2024 2024 Q
+python3 -m scripts.backfill 2025 2025 Q
+python3 -m scripts.backfill 2026 2026 Q
+
+# Build Features
+
+python3 -m scripts.bootstrap_db
+python3 -m scripts.build_features
+
+# Train model
+
+python -m scripts.train_model
